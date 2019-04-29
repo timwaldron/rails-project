5bddf0fafb41ae78a4997861e2ed506c3652a343
Timothy Waldron <tim@waldron.im>
2019-04-30 09:22:39 +1000

    Adjusted game listing, helper methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,20 @@
 class GamesController < ApplicationController
+  helper_method :find_username_by_id
+
   def index
-    @users = User.all
-    p @users
-    
-    @games = Game.all
-    p @games
+    @games = Game.all.order("created_at ASC")
+  end
+
+  def handle_user_notfound
+    return "ERROR"
+  end
+
+  def find_username_by_id(user_id)
+    # user =  User.exists?(id: user_id)
+    if User.exists?(id: user_id)
+      return User.find(user_id).username
+    else
+      return "ERROR"
+    end
   end
 end
