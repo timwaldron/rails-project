d0eebd31a2cf3fd11a16a0cc29c2b73603ee8b7c
Timothy Waldron <tim@waldron.im>
2019-04-29 16:23:59 +1000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Random users
+
+puts 'Generating test accounts...'
+
+params = {
+  first_name: 'Tim',
+  last_name: 'Waldron',
+  email: 'tim@ozgamers.shop',
+  username: 'tim',
+  street_number: '555',
+  street_name: 'Fake Street',
+  suburb: 'Fakeland',
+  city: 'Melbourne',
+  postcode: 3000,
+  date_of_birth: Date.now()
+}
+User.new(params).save
+puts 'Created Tim'
+
+params = {
+  first_name: 'David',
+  last_name: 'Bui',
+  email: 'david@ozgamers.shop',
+  username: 'david',
+  street_number: '555',
+  street_name: 'Fake Street',
+  suburb: 'Fakeland',
+  city: 'Melbourne',
+  postcode: 3000,
+  date_of_birth: Date.now()
+}
+User.new(params).save
+puts 'Created David'
+
+
+puts 'Generating random users...'
+20.times do
+
+  params = {
+    first_name: Faker::Name.unique.first_name,
+    last_name:  Faker::Name.unique.last_name,
+    email: "#{params[:first_name]}@ozgamers.shop",
+    username: params[:first_name],
+    street_number: '555',
+    street_name: 'Fake Street',
+    suburb: 'Fakeland',
+    city: 'Melbourne',
+    postcode: 3000,
+    date_of_birth: Date.now()
+  }
+  
+  User.new(params).save
+
+end
