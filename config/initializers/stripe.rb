be34a626c9cf5cb48893494e07aa1448eac1206b
Timothy Waldron <tim@waldron.im>
2019-05-02 12:50:11 +1000

    stripe init change

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.dig(:stripe_publishable_key),
-  :secret_key      => Rails.application.credentials.dig(:stripe_secret_key)
+  :publishable_key => Rails.application.credentials.stripe[:publishable_key],    
+  :secret_key => Rails.application.credentials.stripe[:secret_key]    
 }
 
-Stripe.api_key = Rails.configuration.stripe[:stripe_secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
