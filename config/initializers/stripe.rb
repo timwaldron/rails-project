a16a874fcc8c5add3886552d510505e0c7c1ee32
Timothy Waldron <tim@waldron.im>
2019-05-02 13:13:04 +1000

    stripe reconfig

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
-  # :publishable_key => Rails.application.credentials.dig(:stripe, :publishable_key),
-  # :secret_key      => Rails.application.credentials.dig(:stripe, :secret_key)
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key             => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => Rails.application.credentials.stripe(:publishable_key),
+  :secret_key      => Rails.application.credentials.stripe(:secret_key)
+  # :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  # :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
