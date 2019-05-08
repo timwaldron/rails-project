6e810ae08706f711c410e63ef1261207c82e0188
Timothy Waldron <tim@waldron.im>
2019-05-08 17:16:37 +1000

    Added bootstrap tables to transaction page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,4 +17,20 @@ module UsersHelper
   def get_game(game_id)
     return Game.find(game_id)
   end
+
+  def get_transaction_history_columns
+    return ["Invoice", "Title", "Platform", "Price", "Transaction Date"]
+  end
+
+  def platform_purchase_hash
+    platform_count = {
+      "PlayStation 4" => 0,
+      "Xbox One" => 0,
+      "PC" => 0,
+      "Nintendo Switch" => 0
+    }
+    ItemTransaction.where(buyer_id: current_user.id).each { |transaction| platform_count[Game.find(transaction.item_id).platform] += 1 }
+
+    return platform_count
+  end
 end
