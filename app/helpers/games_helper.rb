3eb9fab9caa6de2dd6a1bd5fc996bd6ad734b182
Timothy Waldron <tim@waldron.im>
2019-05-09 10:33:42 +1000

    Added OpenSans 400 Google Fonts

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,4 +1,7 @@
 module GamesHelper
+
+  WillPaginate.per_page = 9
+
   def find_username_by_id(user_id)
     
     if User.exists?(id: user_id)
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,5 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-
   self.per_page = 9
 end
-
-WillPaginate.per_page = 9
